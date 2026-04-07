097444323a736bde610ff595f214c05d630ea49b
Keyroguepuma <ajimenez2266@gmail.com>
2026-04-06 20:14:43 -0700

    Added Express js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+
+    res.send("Hello world\n");
+})
+
+app.listen(port, () => {
+
+    console.log(`Listening app on port ${port}`);
+})
