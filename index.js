7026b2c7e2c69e63cb239d273aa268b86470eb23
Keyroguepuma <ajimenez2266@gmail.com>
2026-04-19 11:36:21 -0700

    Set up handlebars

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const hbs = require('express-handlebars');
+require('dotenv').config();
+
+//Set up the variables
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+//For CSS
+app.use(express.static('public'));
+
+//Set Up handlebars
+app.engine('handlebars', hbs.engine({
+  defaultLayout: 'main',
+  layoutsDir: './views/layouts',
+  partialsDir: './views/partials',
+  extname: "hbs"
+}));
+
+//This is where the front end is stored
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 
+//Home page
 app.get('/', (req, res) => {
 
     res.send("Hello world\n");
 })
 
+//Start the server
 app.listen(port, () => {
 
     console.log(`Listening app on port ${port}`);
