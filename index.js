58e2ce42b7ec5c9df8ee44d2ce0c9c546bdcff46
Keyroguepuma <ajimenez2266@gmail.com>
2026-04-19 12:01:56 -0700

    Getting response from user

--- index.js
+++ index.js
@@ -10,22 +10,36 @@ const port = process.env.PORT;
 app.use(express.static('public'));
 
 //Set Up handlebars
-app.engine('handlebars', hbs.engine({
+app.engine('hbs', hbs.engine({
+
   defaultLayout: 'main',
   layoutsDir: './views/layouts',
   partialsDir: './views/partials',
   extname: "hbs"
 }));
 
-//This is where the front end is stored
-app.set('view engine', 'handlebars');
+//This is where the front end is stored (views)
+app.set('view engine', 'hbs');
 app.set('views', './views');
 
+//To get user data
+app.use(express.urlencoded({ extended: true }));
 
 //Home page
 app.get('/', (req, res) => {
 
-    res.send("Hello world\n");
+    res.render('home');
+})
+
+//Getting response from the home page
+app.post('/input', (req, res) => {
+
+    console.log(req.body.medicineInput);
+
+    res.send("Read input");
+
+    /*Here we are going to call get medicine info from openservice.js
+    and then console.log it test it first*/
 })
 
 //Start the server
